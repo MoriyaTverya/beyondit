116214f47c474472b28eae7185bef37da91a1de6
MoriyaTverya <moriya.tverya@kaltura.com>
2024-03-05 12:10:54 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          B E Y O N D I T
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          יֵשׁ כָּאן יוֹתֵר מִזֶּה
+        </p>
       </header>
     </div>
   );
